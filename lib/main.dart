e7cf8fbc303dbefa5e0ac666b2feebb8dfce5e68
ruchithaLiyanage <ruchithal@zone24x7.com>
2026-08-17 21:32:58 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _PlaceholderApp());
+}
+
+class _PlaceholderApp extends StatelessWidget {
+  const _PlaceholderApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Posts App'))),
+    );
+  }
+}
